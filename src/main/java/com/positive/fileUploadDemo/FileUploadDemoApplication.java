e1a5a2bff9fb55ac63eb4f55a187ed714d59b400
Subhranshu Pujari <subh.pujari@gmail.com>
2020-06-27 22:34:17 +0530

    File upload demo

---
+++ src/main/java/com/positive/fileUploadDemo/FileUploadDemoApplication.java
@@ -0,0 +1,13 @@
+package com.positive.fileUploadDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FileUploadDemoApplication {
+ 
+  public static void main(String[] args) {
+    SpringApplication.run(FileUploadDemoApplication.class, args);
+  }
+}
